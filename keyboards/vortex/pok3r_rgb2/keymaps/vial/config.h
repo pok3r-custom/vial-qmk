44e2decae78b66f97655bb14f731d1fdba561ab9
Charlie Waters <cawiii@me.com>
2025-12-28 04:24:47 -0500

    vortex/pok3r_rgb2: Init POK3R RGB 2 based on POK3R RGB

---
+++ keyboards/vortex/pok3r_rgb2/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xBC, 0xF9, 0x07, 0x04, 0x28, 0x35, 0xB0, 0x6B}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+// Enable additional LED functionality
+#define USB_LED_INDICATOR_ENABLE
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
