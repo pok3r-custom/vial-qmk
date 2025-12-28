97d3c297e6f24d4d1de8df283091f7f4f41dc421
Charlie Waters <cawiii@me.com>
2025-12-27 21:32:35 -0500

    vortex/pok3r_rgb: Vial support

---
+++ keyboards/vortex/pok3r_rgb/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x63, 0x04, 0x4C, 0xE6, 0xE5, 0x09, 0xAE, 0xFA}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+// Enable additional LED functionality
+#define USB_LED_INDICATOR_ENABLE
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
