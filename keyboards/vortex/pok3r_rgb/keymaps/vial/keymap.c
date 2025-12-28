83acccbcbfcf7480474d47f2a15b7d13bb651bd7
Charlie Waters <cawiii@me.com>
2025-12-27 21:48:16 -0500

    vortex/pok3r_rgb: Alternate base layer is uneeded with vial

--- keyboards/vortex/pok3r_rgb/keymaps/vial/keymap.c
+++ keyboards/vortex/pok3r_rgb/keymaps/vial/keymap.c
@@ -27,16 +27,8 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(2),   KC_APP,  KC_RCTL
     ),
-    // Caps Lock mapped to FN
-    [1] = LAYOUT_60_ansi(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        MO(2),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(2),   KC_APP,  KC_RCTL
-    ),
     // FN layer
-    [2] = LAYOUT_60_ansi(
+    [1] = LAYOUT_60_ansi(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______, _______, KC_UP,   _______, _______, _______, _______, KC_PGUP, KC_UP,   KC_PGDN, KC_PSCR, KC_SCRL, KC_PAUS, _______,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT,KC_INS,  RGB_SAI,          _______,
@@ -44,26 +36,3 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                            _______, _______, _______, _______
     ),
 };
-
-bool dip_switch_update_user(uint8_t index, bool active) {
-    switch (index) {
-        case 0:
-            /* unused */
-            break;
-        case 1:
-            /* unused */
-            break;
-        case 2:
-            /* map FN to caps lock if dip_switch[2] is on */
-            if (active) {
-                layer_on(1);
-            } else {
-                layer_off(1);
-            }
-            break;
-        case 3:
-            /* unused */
-            break;
-    }
-    return true;
-}
